1e0024d012d125092e4e5d3f567dfdf6e7216007
krawtschenko <eugenykravchenko@gmail.com>
2025-03-11 15:29:54 +0100

    JOY-11 fix: destructuring

--- src/shared/ui/card/Card.tsx
+++ src/shared/ui/card/Card.tsx
@@ -2,11 +2,13 @@ import { ComponentProps } from "react";
 import clsx from "clsx";
 import styles from "./card.module.scss";
 
-export const Card = (props: ComponentProps<"div">) => {
-  const { children, className, ...rest } = props;
-
+export const Card = ({
+  children,
+  className,
+  ...props
+}: ComponentProps<"div">) => {
   return (
-    <div className={clsx(styles.card, className)} {...rest}>
+    <div className={clsx(styles.card, className)} {...props}>
       {children}
     </div>
   );
