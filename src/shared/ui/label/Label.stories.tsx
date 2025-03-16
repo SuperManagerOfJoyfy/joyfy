814427ccc2eaac9efbbfb86633b2a3f8503b79df
aliadamovich <alesya.argentina@gmail.com>
2025-03-16 12:41:40 -0300

    fix: fix required prop in texy-field, add Label story

---
+++ src/shared/ui/label/Label.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Label } from '@/shared/ui/label/Label';
+import { TextField } from '@/shared/ui/textField/TextField';
+
+const meta: Meta<typeof Label> = {
+	title: 'Components/Label',
+	component: Label,
+	tags: ["autodocs"],
+	argTypes: {
+		label: {
+			description: 'The text displayed as the label.',
+			control: 'text',
+		},
+		htmlFor: {
+			description: 'The ID of the input element the label is associated with.',
+			control: 'text',
+		},
+	}
+};
+
+export default meta;
+type Story = StoryObj<typeof Label>;
+
+export const Default: Story = {
+	args: {
+		label: "Email",
+		children: (
+			<TextField />
+		)
+	},
+};
