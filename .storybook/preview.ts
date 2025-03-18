c6613c11ed447db04a24d44ef59ea553262e1491
Veta-s <supermanager2025@gmail.com>
2025-03-18 14:48:37 +0300

    fix: styles & recaptcha

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,26 +3,26 @@ import '../src/app/globals.css'
 import { themes } from '@storybook/theming'
 
 const preview: Preview = {
-    parameters: {
-        actions: { argTypesRegex: '^on[A-Z].*' },
-        docs: {
-            theme: themes.dark,
-        },
-        controls: {
-            matchers: {
-                color: /(background|color)$/i,
-                date: /Date$/i,
-            },
-        },
-        backgrounds: {
-            default: 'dark',
-            values: [
-                { name: 'light', value: '#ffffff' },
-                { name: 'dark', value: '#171717' },
-                { name: 'gray', value: '#333333' },
-            ],
-        },
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    docs: {
+      theme: themes.dark,
     },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    backgrounds: {
+      default: 'dark',
+      values: [
+        { name: 'light', value: 'var(--color-light-100)' },
+        { name: 'dark', value: 'var(--color-dark-700)' },
+        { name: 'gray', value: 'var(--color-dark-100)' },
+      ],
+    },
+  },
 }
 
 export default preview
