a2183f3b6962929ce862b716c617f2275374f3ad
Natalia Yarmolinskaya <nat_yarm@natalias-mbp.home>
2025-03-24 15:33:33 +0100

    add controlled checkbox to signup form. modify signup schema to for the agreeToTerms to accept boolean

--- src/features/auth/signupForm/SignupForm.tsx
+++ src/features/auth/signupForm/SignupForm.tsx
@@ -1,11 +1,18 @@
 'use client'
-import { Path, SubmitHandler } from 'react-hook-form'
+import { Path, SubmitHandler, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { FaGithub } from 'react-icons/fa'
 import { FcGoogle } from 'react-icons/fc'
 import Link from 'next/link'
 import { z } from 'zod'
 
-import { Card, Checkbox, Form, Typography } from '@/shared/ui'
+import {
+  Card,
+  Checkbox,
+  ControlledCheckbox,
+  Form,
+  Typography,
+} from '@/shared/ui'
 import { SignupSchema } from '../utils/schemas/SignupSchema'
 import s from './signupForm.module.scss'
 
@@ -20,21 +27,29 @@ const fields: {
   { name: 'confirmPassword', label: 'Password confirmation', type: 'password' },
 ]
 
-const additionalContent = (
-  <div className={s.contentContainer}>
-    <Checkbox
-      labelClassName={s.checkboxLabel}
-      label="I agree to the Terms of Service and Privacy Policy"
-    />
-  </div>
-)
-
 export const SignupForm = () => {
+  const { control, handleSubmit } = useForm<z.infer<typeof SignupSchema>>({
+    resolver: zodResolver(SignupSchema),
+    defaultValues: { agreeToTerms: false },
+  })
+
   const handleSignupSubmit: SubmitHandler<z.infer<typeof SignupSchema>> = (
     data
   ) => {
     console.log('Form submitted with:', data)
   }
+
+  const additionalContent = (
+    <div className={s.contentContainer}>
+      <ControlledCheckbox
+        control={control}
+        name="agreeToTerms"
+        labelClassName={s.checkboxLabel}
+        label="I agree to the Terms of Service and Privacy Policy"
+      />
+    </div>
+  )
+
   return (
     <Card className={s.card}>
       <Typography as="h1" variant="h1" className={s.title}>
--- src/features/auth/utils/schemas/SignupSchema.ts
+++ src/features/auth/utils/schemas/SignupSchema.ts
@@ -1,32 +1,41 @@
-import {z} from 'zod'
+import { z } from 'zod'
 
-const passwordError = 'Password must contain at least one uppercase letter, one number, and one special character.'
+const passwordError =
+  'Password must contain at least one uppercase letter, one number, and one special character.'
 
-export const SignupSchema = z.object({
-	username: z
-	.string()
-	.min(6, 'Minimum number of characters 6')
-	.max(30, 'Maximum number of characters 30')
-	.regex(/^[0-9A-Za-z_-]+$/, {message: 'Invalid characters entered'}),
+export const SignupSchema = z
+  .object({
+    username: z
+      .string()
+      .min(6, 'Minimum number of characters 6')
+      .max(30, 'Maximum number of characters 30')
+      .regex(/^[0-9A-Za-z_-]+$/, { message: 'Invalid characters entered' }),
 
-	email: z
-	.string()
-	.email('The email must match the format example@example.com'),
+    email: z
+      .string()
+      .email('The email must match the format example@example.com'),
 
-	password: z
-	.string()
-	.min(6, 'Minimum number of characters 6')
-	.max(20, 'Maximum number of characters 20')
-	.regex(/^[0-9A-Za-z!\"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]+$/, {message: passwordError}),
-	
-	confirmPassword: z.string(),
-	
-	agreeToTerms: z
-	.literal(true, {errorMap: () => ({message: 'You must agree to the Terms & Conditions'})} )
-	
-}).refine( (arg) => arg.password ===arg.confirmPassword, {
-	message: 'Your passwords don’t match. Try again.',
-	path: ['confirmPassword']
-})
+    password: z
+      .string()
+      .min(6, 'Minimum number of characters 6')
+      .max(20, 'Maximum number of characters 20')
+      .regex(/^[0-9A-Za-z!\"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]+$/, {
+        message: passwordError,
+      }),
+
+    confirmPassword: z.string(),
+
+    agreeToTerms: z
+      .boolean()
+      .default(false)
+      .catch(false)
+      .refine((val) => val === true, {
+        message: 'You must agree to the Terms & Conditions',
+      }),
+  })
+  .refine((arg) => arg.password === arg.confirmPassword, {
+    message: 'Your passwords don’t match. Try again.',
+    path: ['confirmPassword'],
+  })
 
 type SignupSchema = z.infer<typeof SignupSchema>
