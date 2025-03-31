59be4e559984e85a6c2d929092558e5af0641061
NatYarm <yarmnat7@gmail.com>
2025-04-01 00:11:37 +0200

    start homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import { Typography } from '@/shared/ui/typography'
+import { Card } from '@/shared/ui/card'
 
 export default function Home() {
   return (
-    <div>
-      Public Page Here !!!
+    <div className="container">
+      <Card>
+        <Typography as="h2" fontWeight="bold">
+          Registered users:
+        </Typography>
+      </Card>
     </div>
   )
 }
