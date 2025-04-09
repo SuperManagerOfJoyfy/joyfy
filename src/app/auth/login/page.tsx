9011930d6e5ed7d7c8a1ec9a99fc3e1b561827e7
krawtschenko <eugenykravchenko@gmail.com>
2025-04-09 20:59:05 +0200

    JOY-19-fix: The issue where authentication was successful but the unauthorized user page was displayed has been fixed. Let me know if you need any refinements!

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -5,17 +5,22 @@ import { useLoginMutation } from '@/features/auth/api/authApi'
 import { LoginFormValues } from '@/features/auth/ui/login'
 import { useRouter } from 'next/navigation'
 import { PATH } from '@/shared/config/routes'
+import { useAuth } from '@/features/auth/hooks/useAuth'
+import { toast } from 'react-toastify'
 
 const Page = () => {
   const [login] = useLoginMutation()
   const router = useRouter()
+  const { refetchUser } = useAuth()
 
   async function handleLogin(data: LoginFormValues) {
     try {
-      await login(data)
+      await login(data).unwrap()
+      await refetchUser()
+
       router.push(PATH.ROOT)
     } catch (error) {
-      console.error(error)
+      toast.error('Could not sign in')
     }
   }
 
