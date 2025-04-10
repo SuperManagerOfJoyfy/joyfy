6798c5cfd80d56b0889b55c93cd6fd9541424abd
Inna Dmytrenko <inna.shalun@gmail.com>
2025-04-10 14:52:40 +0200

    sidebar: fix activeIcon

--- src/shared/utils/sidebarItem/SidebarItem.tsx
+++ src/shared/utils/sidebarItem/SidebarItem.tsx
@@ -12,6 +12,22 @@ import {
   FiUser,
   FiUsers,
 } from 'react-icons/fi'
+
+import {
+  FaHome,
+  FaChartBar,
+  FaCreditCard,
+  FaImage,
+  FaSignOutAlt,
+  FaComments,
+  FaPlusCircle,
+  FaSearch,
+  FaStar,
+  FaChartLine,
+  FaUser,
+  FaUsers,
+} from 'react-icons/fa'
+
 import { SidebarItem } from '../../ui/sidebar'
 import { PATH } from '@/shared/config/routes'
 
@@ -30,24 +46,28 @@ export const createSidebarItems = (
       title: 'Users list',
       path: PATH.ADMIN.USERS,
       icon: <FiUsers />,
+      activeIcon: <FaUsers />,
     },
     {
       id: 'stats',
       title: 'Statistics',
       path: PATH.ADMIN.STATISTICS,
       icon: <FiTrendingUp />,
+      activeIcon: <FaChartLine />,
     },
     {
       id: 'payments',
       title: 'Payments list',
       path: PATH.ADMIN.PAYMENTS,
       icon: <FiCreditCard />,
+      activeIcon: <FaCreditCard />,
     },
     {
       id: 'posts',
       title: 'Posts list',
       path: PATH.ADMIN.POSTS,
       icon: <FiImage />,
+      activeIcon: <FaImage />,
     },
   ]
 
@@ -57,36 +77,42 @@ export const createSidebarItems = (
       title: 'Home',
       path: PATH.ROOT,
       icon: <FiHome />,
+      activeIcon: <FaHome />,
     },
     {
       id: 'create',
       title: 'Create',
       path: PATH.USER.CREATE,
       icon: <FiPlusCircle />,
+      activeIcon: <FaPlusCircle />,
     },
     {
       id: 'profile',
       title: 'My Profile',
       path: PATH.USER.PROFILE,
       icon: <FiUser />,
+      activeIcon: <FaUser />,
     },
     {
       id: 'messenger',
       title: 'Messenger',
       path: PATH.USER.MESSENGER,
       icon: <FiMessageCircle />,
+      activeIcon: <FaComments />,
     },
     {
       id: 'search',
       title: 'Search',
       path: PATH.USER.SEARCH,
       icon: <FiSearch />,
+      activeIcon: <FaSearch />,
     },
     {
       id: 'statistics',
       title: 'Statistics',
       path: PATH.USER.STATISTICS,
       icon: <FiBarChart2 />,
+      activeIcon: <FaChartBar />,
       style: { marginTop: '60px' },
     },
     {
@@ -94,12 +120,14 @@ export const createSidebarItems = (
       title: 'Favorites',
       path: PATH.USER.FAVORITES,
       icon: <FiStar />,
+      activeIcon: <FaStar />,
       style: { marginBottom: '180px' },
     },
     {
       id: 'logout',
       title: 'Log Out',
       icon: <FiLogOut />,
+      activeIcon: <FaSignOutAlt />,
       onClick: handlers.openLogoutModal,
       className: 'logoutItem',
     },
