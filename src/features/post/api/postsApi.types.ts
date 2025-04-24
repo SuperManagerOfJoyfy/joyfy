ba266b5d04f5718f32d212cc0f933cc1e63b1e13
aliadamovich <alesya.argentina@gmail.com>
2025-04-24 16:15:17 -0300

    This reverts commit a3512f8c621a805c5b4a31d3843b89520d010d4b.

---
+++ src/features/post/api/postsApi.types.ts
@@ -0,0 +1,36 @@
+type Image = {
+	url: string
+	width: number
+	height: number
+	fileSize: number
+	createdAt: string
+	uploadId: string
+}
+
+type Owner = {
+	firstName: string
+	lastName: string
+}
+
+type Item = {
+	id: number
+	userName: string
+	description: string
+	location: string
+	images: Image[]
+	createdAt: string
+	updatedAt: string
+	ownerId: number
+	avatarOwner: string
+	owner: Owner
+	likesCount: number
+	isLiked: boolean
+	avatarWhoLikes: boolean
+}
+
+export type GetAllPostsResponse= {
+	pageSize: number
+	totalCount: number
+	notReadCount: number
+	items: Item[]
+}
