26b1fdfd1cd7086d5d8f653fec9a89a60388bcc6
aliadamovich <alesya.argentina@gmail.com>
2025-04-24 15:38:29 -0300

    faet: create postsApi, profileApi

---
+++ src/features/post/api/postsApi.ts
@@ -0,0 +1,14 @@
+import { GetAllPostsResponse } from "@/features/post/api/postsApi.types"
+import { joyfyApi } from "@/shared/api/joyfyApi"
+
+export const postsApi = joyfyApi.injectEndpoints({
+	endpoints: (builder) => ({
+		getAllPosts: builder.query<GetAllPostsResponse, {userName: string}>({
+			query: ({userName}) => ({
+				url: `/posts/${userName}`,
+				method: 'GET',
+			}),
+			providesTags: ['Posts'],
+		}),
+  }),
+})
