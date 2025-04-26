4157c4f4b4471758849d9a58b4f093a13b537a2b
Alesya Adamovich <131908857+aliadamovich@users.noreply.github.com>
2025-04-26 04:49:25 -0300

    Joy 45 fix; watch all posts (use Auth) (#90)
    * fix: standartize layout styles
    
    * faet: create postsApi, profileApi
    
    * Revert "faet: create postsApi, profileApi"
    
    This reverts commit 26b1fdfd1cd7086d5d8f653fec9a89a60388bcc6.
    
    * This reverts commit a3512f8c621a805c5b4a31d3843b89520d010d4b.
    
    * Revert "This reverts commit a3512f8c621a805c5b4a31d3843b89520d010d4b."
    
    This reverts commit ba266b5d04f5718f32d212cc0f933cc1e63b1e13.
    
    * create postsApi, profileApi
    
    * test commit
    
    * feat: create PostGrid
    
    * fix: add info msg when no posts
    
    * fix: add useAuth, fix PostsGrid
    
    ---------
    
    Co-authored-by: Inna Dmytrenko <inna.shalun@gmail.com>

--- src/features/post/ui/createPost/postsGrid/PostsGrid.tsx
+++ src/features/post/ui/createPost/postsGrid/PostsGrid.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 
 import React, { useEffect } from 'react'
 import s from './PostsGrid.module.scss'
@@ -7,47 +7,55 @@ import { PostItem } from '@/features/post/types/types'
 import { Loader } from '@/shared/ui/loader/Loader'
 
 type Props = {
-	posts: PostItem[]
-	onPostClick?: () => void
-	loadMorePosts?: () => void
-	isFetching?: boolean
-	hasNextPage?: boolean
+  posts: PostItem[]
+  onPostClick?: () => void
+  loadMorePosts?: () => void
+  isFetching?: boolean
+  hasNextPage?: boolean
 }
 
-export const PostsGrid = ({ onPostClick, posts, loadMorePosts, isFetching, hasNextPage }: Props) => {
+export const PostsGrid = ({
+  onPostClick,
+  posts,
+  loadMorePosts,
+  isFetching,
+  hasNextPage,
+}: Props) => {
+  useEffect(() => {
+    const handleScroll = () => {
+      if (isFetching || !hasNextPage) return
 
-	useEffect(() => {
-		const handleScroll = () => {
-			if (isFetching || !hasNextPage) return;
+      const { scrollTop, scrollHeight, clientHeight } = document.documentElement
+      if (scrollTop + clientHeight >= scrollHeight - 100) {
+        loadMorePosts?.()
+      }
+    }
+    window.addEventListener('scroll', handleScroll)
 
-			const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-			if (scrollTop + clientHeight >= scrollHeight - 100) {
-				loadMorePosts?.()
-			}
-		}
-		window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [loadMorePosts, hasNextPage, isFetching])
 
-		return () => {
-			window.removeEventListener('scroll', handleScroll)
-		}
-	}, [loadMorePosts, hasNextPage, isFetching])
-
-	return (
-		<div>
-			<div className={s.gridContainer}>
-			{
-				posts?.map(post =>
-					<div className={s.gridItem} onClick={onPostClick}>
-						<Image src={post.images[0].url} alt='post image' />
-					</div>)
-			}
-			</div>
-			{isFetching && (
-				<div className={s.loaderContainer}>
-					<Loader />
-				</div>
-			)}
-		</div>
-	)
+  return (
+    <div>
+      <div className={s.gridContainer}>
+        {posts?.map((post) => (
+          <div className={s.gridItem} onClick={onPostClick} key={post.id}>
+            <Image
+              src={post.images[0].url}
+              alt="post image"
+              width={235}
+              height={228}
+            />
+          </div>
+        ))}
+      </div>
+      {isFetching && (
+        <div className={s.loaderContainer}>
+          <Loader />
+        </div>
+      )}
+    </div>
+  )
 }
-
--- src/features/post/ui/createPost/postsGrid/PostsGridWithInfinteScroll.tsx
+++ src/features/post/ui/createPost/postsGrid/PostsGridWithInfinteScroll.tsx
@@ -1,22 +1,24 @@
-import { selectUserName } from '@/features/auth/model/authSlice'
+import { useAuth } from '@/features/auth/hooks/useAuth'
 import { useGetAllPostsQuery } from '@/features/post/api/postsApi'
 import { PostsGrid } from '@/features/post/ui/createPost/postsGrid/PostsGrid'
+import { Loader } from '@/shared/ui/loader/Loader'
 import { skipToken } from '@reduxjs/toolkit/query'
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 
 export const PostsGridWithInfinteScroll = () => {
+  const { user } = useAuth()
+  const { data, isLoading } = useGetAllPostsQuery(
+    user ? { userName: user.userName } : skipToken
+  )
 
-	const userName = useSelector(selectUserName)
-	const { data } = useGetAllPostsQuery(userName ? { userName } : skipToken)
+  const posts = data?.items
+	if (isLoading) return <Loader />
+  if (!posts || posts.length === 0) return <div>No posts yet...</div>
 
-	const posts = data?.items
-
-	if (!posts || posts.length === 0) return <div>No posts yet...</div>;
-
-	return (
-		<div>
-			<PostsGrid posts={posts}/>
-		</div>
-	)
+  return (
+    <div>
+      <PostsGrid posts={posts} />
+    </div>
+  )
 }
