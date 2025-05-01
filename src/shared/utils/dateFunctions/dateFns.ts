4b84f8e7a07532d48308a57219085fb79ba1fcb7
NatYarm <yarmnat7@gmail.com>
2025-05-02 00:55:37 +0200

    Add smartFormatDate fn to display the date in the format "MMMM dd YYYY" if it happened more than 2 days ago and in the format 'time ago' if less

--- src/shared/utils/dateFunctions/dateFns.ts
+++ src/shared/utils/dateFunctions/dateFns.ts
@@ -1,4 +1,4 @@
-import { format, formatDistanceToNow } from 'date-fns'
+import { format, formatDistanceToNow, parseISO, subDays } from 'date-fns'
 
 export const timeAgoFn = (date: string) => {
   return formatDistanceToNow(new Date(date), { addSuffix: true })
@@ -6,4 +6,15 @@ export const timeAgoFn = (date: string) => {
 
 export const formatDate = (date: string) => {
   return format(new Date(date), 'MMMM d, yyyy')
+}
+
+export const formatSmartDate = (dateString: string) => {
+  const date = parseISO(dateString)
+  const twoDaysAgo = subDays(new Date(), 2)
+
+  if (date > twoDaysAgo) {
+    return timeAgoFn(dateString)
+  }
+
+  return formatDate(dateString)
 }
