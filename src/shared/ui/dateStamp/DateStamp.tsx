dd742fe21f5aa6c90c38d1d1ffc91c81f42304ba
NatYarm <yarmnat7@gmail.com>
2025-05-05 02:49:26 +0200

    Add DateStamp component and stories. Add Separator component.

--- src/shared/ui/dateStamp/DateStamp.tsx
+++ src/shared/ui/dateStamp/DateStamp.tsx
@@ -1,10 +1,12 @@
 import { formatSmartDate } from '@/shared/utils/dateFunctions'
+import clsx from 'clsx'
 import s from './DateStamp.module.scss'
 
 type Props = {
   date: string
+  className?: string
 }
 
-export const DateStamp = ({ date }: Props) => {
-  return <span className={s.date}>{formatSmartDate(date)}</span>
+export const DateStamp = ({ date, className }: Props) => {
+  return <div className={clsx(s.date, className)}>{formatSmartDate(date)}</div>
 }
