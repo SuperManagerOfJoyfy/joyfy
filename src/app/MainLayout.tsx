a7ce85337d6440c90ba01b6e251e9b2580f86312
pobaranchuk <49860684+pobaranchuk@users.noreply.github.com>
2025-05-06 14:59:37 +0200

    JOY-80 feat: Рефакторинг обработки выхода и улучшение структуры модуля при логауте. Удалён хук useLogout, его функциональность интегрирована напрямую в authApi для более чистой архитектуры. Обновлена структура папок и пути к компонентам для лучшей организации. Внесены небольшие изменения в UI стилях и исправлены проблемы, связанные с валидацией токена при выходе из системы (#106)
    Co-authored-by: VPobaranchuk <vpobaranchuk@gmail.com>

--- src/app/MainLayout.tsx
+++ src/app/MainLayout.tsx
@@ -8,7 +8,6 @@ import { Header } from '@/shared/ui/header/Header'
 import { Loader } from '@/shared/ui/loader/Loader'
 import { createSidebarItems } from '@/shared/utils/sidebarItem/SidebarItem'
 import s from '../styles/layout.module.scss'
-import { useLogout } from '@/features/auth/hooks/useLogout'
 import { useAuth } from '@/features/auth/hooks/useAuth'
 
 type MainLayoutProps = {
@@ -19,7 +18,6 @@ export default function MainLayout({ children }: MainLayoutProps) {
   const pathname = usePathname()
 
   const { user, isAppInitialized } = useAuth()
-  const { logoutUser } = useLogout()
 
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [pendingPath, setPendingPath] = useState<string | null>(null)
@@ -50,11 +48,13 @@ export default function MainLayout({ children }: MainLayoutProps) {
     )
   }
 
+  const isUserToken = localStorage.getItem('accessToken')
+
   return (
     <div className={s.layoutWrapper}>
       <Header />
       <div className={s.containerLayout}>
-        {user && (
+        {isUserToken && (
           <div className={s.sidebarContainer}>
             <Sidebar
               items={sidebarItems}
@@ -63,7 +63,7 @@ export default function MainLayout({ children }: MainLayoutProps) {
                 if (item.path) setPendingPath(item.path)
               }}
             />
-            <LogoutModal open={isModalOpen} onOpenLogoutModalHandler={onOpenLogoutModalHandler} onLogout={logoutUser} />
+            <LogoutModal open={isModalOpen} onOpenLogoutModalHandler={onOpenLogoutModalHandler} />
           </div>
         )}
 
