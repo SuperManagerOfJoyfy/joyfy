dd4d9961841deeecbd9398a044c6dba5ad8d2d2d
Eugene Kravchenko <eugenykravchenko@gmail.com>
2025-05-10 19:17:02 +0200

    feat-reusable-confirmModal (#116)

---
+++ src/shared/ui/confirmModal/ConfirmModal.tsx
@@ -0,0 +1,35 @@
+import s from './confirmModal.module.scss'
+import { Button, Modal, Typography } from '@/shared/ui'
+
+type Props = {
+  title: string
+  description: string
+  isOpen: boolean
+  setIsOpen: (isOpen: boolean) => void
+  onConfirm: () => void
+}
+
+export const ConfirmModal = ({ title, description, isOpen, setIsOpen, onConfirm }: Props) => {
+  function handleConfirm() {
+    onConfirm()
+    setIsOpen(false)
+  }
+
+  return (
+    <Modal title={title} className={s.confirmModal} open={isOpen} onOpenChange={setIsOpen}>
+      <div className={s.content}>
+        <Typography>{description}</Typography>
+
+        <div className={s.buttons}>
+          <Button className={s.button} variant={'outline'} onClick={handleConfirm}>
+            Yes
+          </Button>
+
+          <Button className={s.button} onClick={() => setIsOpen(false)}>
+            No
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  )
+}
