8000991ad2ec18fe6b1e6c5facbdf13826e0c5ab
NatYarm <yarmnat7@gmail.com>
2025-05-13 22:38:01 +0200

    Add PostContent component to render read-only post and EditPostForm to render form for editing publication. Add post edit functionality. Modify ConfirmModal to handle post delete and cancel edit actions. Add querries to getPostById and to editPost.

--- src/shared/ui/userCard/UserCard.tsx
+++ src/shared/ui/userCard/UserCard.tsx
@@ -1,8 +1,9 @@
 import Link from 'next/link'
 import { Avatar } from '@/shared/ui'
 import s from './UserCard.module.scss'
+import clsx from 'clsx'
 
-type User = {
+export type User = {
   id: number
   userName: string
   avatar?: string | null
@@ -10,12 +11,13 @@ type User = {
 
 type Props = {
   user: User
+  className?: string
 }
 
-export const UserCard = ({ user }: Props) => {
+export const UserCard = ({ user, className }: Props) => {
   const { avatar, id, userName } = user
   return (
-    <div className={s.userInfo}>
+    <div className={clsx(s.userInfo, className)}>
       <Avatar avatar={avatar} />
       <Link href={`/profile/${id}`} className={s.userName}>
         {userName}
