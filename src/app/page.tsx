b165ed80af163f94459f8839615c9b620d13cb30
aliadamovich <alesya.argentina@gmail.com>
2025-05-18 12:43:52 -0300

    fix: add to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
-'use client'
-
+import EmailConfirmation from '@/app/(registration-confirmation)/page'
 import { Card, Typography } from '@/shared/ui'
 
-export default function HomePage() {
+type Props = {
+  searchParams: {
+    code?: string
+  }
+}
+export default function HomePage({ searchParams }: Props) {
+  const code = searchParams.code
+
+  if (code) {
+    return <EmailConfirmation code={code} />
+  }
+
   return (
     <div>
       <Card>
