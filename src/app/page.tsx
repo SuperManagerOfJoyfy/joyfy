cfe2551ef135916ae460ce766b3023247d037194
krawtschenko <eugenykravchenko@gmail.com>
2025-05-22 11:36:31 +0200

    feat: get total count registered users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,45 @@
-import EmailConfirmation from '@/app/(registration-confirmation)/page'
 import { Card, Typography } from '@/shared/ui'
+import s from './page.module.scss'
 
-type Props = {
-  searchParams: {
-    code?: string
-  }
-}
-export default async function HomePage({ searchParams }: Props) {
-  const { code } = await searchParams
+export const revalidate = 60
 
-  if (code) {
-    return <EmailConfirmation code={code} />
+export default async function HomePage() {
+  let count = 0
+
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/public-user`, {
+      next: { revalidate: 60 },
+    })
+
+    if (!res.ok) throw new Error('Failed to fetch user count')
+    const data = await res.json()
+    count = data.totalCount
+  } catch (error) {
+    console.error(error)
   }
 
   return (
     <div>
-      <Card>
+      <Card className={s.card}>
         <Typography as="h2" fontWeight="bold">
           Registered users:
         </Typography>
+
+        <div className={s.countUsersWrapper}>
+          {formatNumberToSixDigits(count).map((n, i) => {
+            return (
+              <Typography key={i} variant="h2" fontWeight="bold" className={s.number}>
+                {n}
+              </Typography>
+            )
+          })}
+        </div>
       </Card>
     </div>
   )
+}
+
+function formatNumberToSixDigits(num: number) {
+  const formatted = num.toString().padStart(6, '0')
+  return [...formatted]
 }
