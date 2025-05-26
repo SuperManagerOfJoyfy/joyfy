1b1460d93d4f4d83d2069ccb0803b1b47beddd4e
VPobaranchuk <vpobaranchuk@gmail.com>
2025-05-26 08:48:42 +0200

    JOY-90 fix: revert unnecessary commits

--- src/features/post/ui/postModal/PostModal.tsx
+++ src/features/post/ui/postModal/PostModal.tsx
@@ -13,7 +13,7 @@ import { Post } from '@/features/post/types/types'
 import s from './PostModal.module.scss'
 
 type Props = {
-  initialPost: Post | null
+  initialPost: Post
 }
 
 type ConfirmAction = 'delete' | 'cancelEdit' | null
