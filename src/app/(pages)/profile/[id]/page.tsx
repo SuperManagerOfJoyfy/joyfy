c71c450a3dec479b081ffd6da0042e19cf62c409
NatYarm <yarmnat7@gmail.com>
2025-05-27 01:31:11 +0200

    refactor: decouple post fetching from ProfilePage and delegate to parallel routes
    - Removed post data fetching logic from ProfilePage
    - Moved responsibility to intercepting route in @modal
    - Improved separation of concerns and reduced unnecessary data loading

--- src/app/(pages)/profile/[id]/page.tsx
+++ src/app/(pages)/profile/[id]/page.tsx
@@ -1,34 +1,27 @@
-import { UserProfile } from '@/features/profile/ui/userProfile'
-import { GetPostsResponse } from '@/features/post/api/postsApi.types'
+import { getUserPosts, getUserProfile } from '@/app/(lib)/dataService'
 import { PostsGridWithInfiniteScroll } from '@/features/post/ui/postsGridWithInfiniteScroll/PostsGridWithInfiniteScroll'
-import { PostModal } from '@/features/post/ui/postModal'
-import { Post } from '@/features/post/types/types'
-import { PublicUserProfile } from '@/features/profile/api/profileApi.types'
+import { UserProfile } from '@/features/profile/ui/userProfile'
 
 type PageProps = {
   params: Promise<{ id: string }>
-  searchParams?: Promise<{ postId?: number }>
 }
 
-export default async function Page({ params, searchParams }: PageProps) {
+export async function generateMetadata({ params }: PageProps) {
   const { id } = await params
-  const { postId } = (await searchParams) || {}
+  const { userName } = await getUserProfile(id)
 
-  const [userRes, postRes, postsRes] = await Promise.all([
-    fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/public-user/profile/${id}`),
-    fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/public-posts/${postId}`),
-    fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/public-posts/user/${id}?pageSize=8`),
-  ])
+  return { title: `Joyfy: ${userName}` }
+}
 
-  const userData: PublicUserProfile = await userRes.json()
-  const post: Post = await postRes.json()
-  const posts: GetPostsResponse = await postsRes.json()
+export default async function ProfilePage({ params }: PageProps) {
+  const { id } = await params
+
+  const [userData, posts] = await Promise.all([getUserProfile(id), getUserPosts(id)])
 
   return (
     <div>
       <UserProfile {...userData} />
       <PostsGridWithInfiniteScroll userId={userData.id} initialPosts={posts} />
-      {!!postId && <PostModal userId={userData.id} initialPost={post} />}
     </div>
   )
 }
