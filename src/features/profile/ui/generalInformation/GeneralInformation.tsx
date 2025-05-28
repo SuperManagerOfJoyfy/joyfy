68cf0d89cc2b05687eaf25f93e3eb909571460a0
aliadamovich <alesya.argentina@gmail.com>
2025-05-28 18:51:03 -0300

    fix: general info fixing

--- src/features/profile/ui/generalInformation/GeneralInformation.tsx
+++ src/features/profile/ui/generalInformation/GeneralInformation.tsx
@@ -1,6 +1,6 @@
-import { CreateProfileForm } from './createProfileForm'
-
 import { ProfilePhoto } from '@/features/profile/ui/profilePhoto/ui/ProfilePhoto'
+import { CreateProfileForm } from './createProfileForm'
+import s from './GeneralInformation.module.scss'
 
 type GeneralInformationProps = {
   userId: number
@@ -8,7 +8,7 @@ type GeneralInformationProps = {
 
 export const GeneralInformation = ({ userId }: GeneralInformationProps) => {
   return (
-    <div>
+    <div className={s.generalContainer}>
       <ProfilePhoto />
       <CreateProfileForm />
     </div>
