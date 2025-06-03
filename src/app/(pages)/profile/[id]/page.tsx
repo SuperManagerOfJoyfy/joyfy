dbd9d7f10ec94717202c0e7ceb10b74e628e1f02
VPobaranchuk <vpobaranchuk@gmail.com>
2025-06-03 17:09:57 +0200

    JOY-91 fix: refactor code for better performance, replace command in jenkins file for deploy

--- src/app/(pages)/profile/[id]/page.tsx
+++ src/app/(pages)/profile/[id]/page.tsx
@@ -2,7 +2,6 @@ import { UserProfile } from '@/features/profile/ui/userProfile'
 import { GetPostsResponse } from '@/features/post/api/postsApi.types'
 import { PostsGridWithInfiniteScroll } from '@/features/post/ui/postsGridWithInfiniteScroll/PostsGridWithInfiniteScroll'
 import { PostModal } from '@/features/post/ui/postModal'
-import { Post } from '@/features/post/types/types'
 import { PublicUserProfile } from '@/features/profile/api/profileApi.types'
 
 type PageProps = {
@@ -14,14 +13,20 @@ export default async function Page({ params, searchParams }: PageProps) {
   const { id } = await params
   const { postId } = (await searchParams) || {}
 
-  const [userRes, postRes, postsRes] = await Promise.all([
+  let post = null
+
+  if (postId) {
+    const postRes = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/public-posts/${postId}`)
+    post = await postRes.json()
+  }
+
+  const [userRes, postsRes] = await Promise.all([
     fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/public-user/profile/${id}`),
-    fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/public-posts/${postId}`),
     fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/public-posts/user/${id}?pageSize=8`),
   ])
 
   const userData: PublicUserProfile = await userRes.json()
-  const post: Post = await postRes.json()
+
   const posts: GetPostsResponse = await postsRes.json()
 
   return (
