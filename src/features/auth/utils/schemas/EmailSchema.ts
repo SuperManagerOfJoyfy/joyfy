4816996e1f0c9bcb1065c3fd855189c90e36a3cc
Alesya Adamovich <131908857+aliadamovich@users.noreply.github.com>
2025-06-08 05:58:01 -0300

    Joy 88 fix registration confirmation (#166)
    * fix: change confirmation routes and logic
    
    * fix: add to homepage
    
    * fix: await params
    
    * fix: amend logic
    
    * fix: delete confirmation from home page
    
    * feat: create inkexpired, fix confirmation

--- src/features/auth/utils/schemas/EmailSchema.ts
+++ src/features/auth/utils/schemas/EmailSchema.ts
@@ -1,3 +1,9 @@
 import { z } from 'zod'
 
 export const EmailSchema = z.string().email('Email must match format example@example.com')
+
+type EmailSchema = z.infer<typeof EmailSchema>
+
+export const EmailFormSchema = z.object({
+  email: EmailSchema,
+})
