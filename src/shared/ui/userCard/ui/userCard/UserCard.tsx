71bb2308c6dc5e4ba559be5842289cfc9fdc473a
NatYarm <yarmnat7@gmail.com>
2025-08-19 22:16:30 +0200

    Modify UserCard by adding timeStamp to stacked layout (for use in messages).

--- src/shared/ui/userCard/ui/userCard/UserCard.tsx
+++ src/shared/ui/userCard/ui/userCard/UserCard.tsx
@@ -1,6 +1,7 @@
+import { Avatar } from '@/shared/ui'
+import { formatChatTimestamp } from '@/shared/utils/dateFunctions'
 import clsx from 'clsx'
 import Link from 'next/link'
-import { Avatar } from '@/shared/ui'
 import { User } from '../../types/userCard.types'
 import s from './UserCard.module.scss'
 
@@ -8,19 +9,23 @@ type Props = {
   user: User
   className?: string
   layout?: 'standalone' | 'inline' | 'stacked' //  'standalone' for use in headers, 'inline' - inside comments/posts, 'stacked' - messages
+  date?: string
   children?: React.ReactNode
 }
 
-export const UserCard = ({ user, className, layout = 'standalone', children }: Props) => {
+export const UserCard = ({ user, className, date, layout = 'standalone', children }: Props) => {
   const { avatar, id, userName } = user
   return (
     <div className={clsx(s.userInfo, s[layout], className)}>
       <Avatar avatar={avatar} name={userName} />
       <div className={s.textBlock}>
-        <Link href={`/profile/${id}`} className={s.userName}>
-          {userName}
-        </Link>
-        {children}
+        <div className={s.headerRow}>
+          <Link href={`/profile/${id}`} className={s.userName}>
+            {userName}
+          </Link>
+          {layout === 'stacked' && <span className={s.timeStamp}>{formatChatTimestamp(date ?? '')}</span>}
+        </div>
+        <p className={s.childrenText}> {children}</p>
       </div>
     </div>
   )
